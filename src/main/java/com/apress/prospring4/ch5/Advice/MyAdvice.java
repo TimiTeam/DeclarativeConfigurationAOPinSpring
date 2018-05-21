8246cc1ac04623e91c519d1e0fbbb818f502c35e
Timur <timatiblackstar666@gmail.com>
2018-05-21 20:02:41 +0300

    Slightly complicated example using namespace aop

--- src/main/java/com/apress/prospring4/ch5/Advice/MyAdvice.java
+++ src/main/java/com/apress/prospring4/ch5/Advice/MyAdvice.java
@@ -3,15 +3,15 @@ package com.apress.prospring4.ch5.Advice;
 
 import org.aspectj.lang.JoinPoint;
 
-public class MyAdvice{
+public class MyAdvice {
 //    If we using namespace aop, we don't need interface 'MethodBeforeAdvice'
 
-    public void simpleBeforeAdvice(JoinPoint joinPoint){
+    public void simpleBeforeAdvice(JoinPoint joinPoint, int intValue) {
 //        If the advice needs access to the details of the cut we use JoinPoint
 //        The arguments on this method arte not necessary
-
-        System.out.println("Executing: "+joinPoint.getSignature().getDeclaringTypeName()+" "
-        +joinPoint.getSignature().getName());
+        if (intValue != 100) {
+            System.out.println("Executing: " + joinPoint.getSignature().getDeclaringTypeName() + " "
+                    + joinPoint.getSignature().getName() + ", argument: " + intValue);
+        }
     }
-
 }
