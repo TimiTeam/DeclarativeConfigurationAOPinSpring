546faa21c45230a5ba90729473aa98ca5acb89f2
Timur <timatiblackstar666@gmail.com>
2018-05-21 19:47:44 +0300

    Adding comments to the code

--- src/main/java/com/apress/prospring4/ch5/Advice/MyAdvice.java
+++ src/main/java/com/apress/prospring4/ch5/Advice/MyAdvice.java
@@ -4,8 +4,11 @@ package com.apress.prospring4.ch5.Advice;
 import org.aspectj.lang.JoinPoint;
 
 public class MyAdvice{
+//    If we using namespace aop, we don't need interface 'MethodBeforeAdvice'
 
     public void simpleBeforeAdvice(JoinPoint joinPoint){
+//        If the advice needs access to the details of the cut we use JoinPoint
+//        The arguments on this method arte not necessary
 
         System.out.println("Executing: "+joinPoint.getSignature().getDeclaringTypeName()+" "
         +joinPoint.getSignature().getName());
